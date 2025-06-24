4121cc56cdc6f199f89b5aeb6d4844cb710a7e08
vtdweller <Weller.devin@gmail.com>
2025-06-23 21:59:53 -0400

    Update streamlit_app.py
    added smoothiefroot API request

--- streamlit_app.py
+++ streamlit_app.py
@@ -47,3 +47,8 @@ if ingredients_list:
         st.success('Your Smoothie is ordered!', icon="✅")
 
 
+import requests
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+st.text(smoothiefroot_response)
+
+
